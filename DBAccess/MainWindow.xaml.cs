d619eb9aa1f4a21f74dea4f8835a5a3938342835
David Mosley <davmmosl@ut.utm.edu>
2020-09-28 12:26:21 -0500

    Pushing Changes

--- DBAccess/MainWindow.xaml.cs
+++ DBAccess/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.OleDb;
 
 namespace DBAccess
 {
@@ -20,9 +21,57 @@ namespace DBAccess
     /// </summary>
     public partial class MainWindow : Window
     {
+        OleDbConnection cn;
         public MainWindow()
         {
             InitializeComponent();
+            cn = new OleDbConnection(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\Access DBs.accdb");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Assets";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + "\n";
+            }
+            AssetsData.Text = data;
+            cn.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            DBAccess.Access_DBs access_DBs = ((DBAccess.Access_DBs)(this.FindResource("access_DBs")));
+            // Load data into the table Assets. You can modify this code as needed.
+            DBAccess.Access_DBsTableAdapters.AssetsTableAdapter access_DBsAssetsTableAdapter = new DBAccess.Access_DBsTableAdapters.AssetsTableAdapter();
+            access_DBsAssetsTableAdapter.Fill(access_DBs.Assets);
+            System.Windows.Data.CollectionViewSource assetsViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("assetsViewSource")));
+            assetsViewSource.View.MoveCurrentToFirst();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string query = "select * from Employees";
+            OleDbCommand cmd = new OleDbCommand(query, cn);
+            cn.Open();
+            OleDbDataReader read = cmd.ExecuteReader();
+            string data = "";
+            while (read.Read())
+            {
+                data += read[0].ToString() + " " + read[1].ToString() + " " + read[2].ToString() + "\n";
+            }
+            AssetsData.Text = data;
+            cn.Close();
         }
     }
 }
